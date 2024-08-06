ea06d2b20facd8d27659c5acfefe17650e4870bd
TheGreatMegalodon <69216391+TheGreatMegalodon@users.noreply.github.com>
2024-08-06 20:10:44 +0200

    Update BaseTeleporter.js

--- BaseTeleporter.js
+++ BaseTeleporter.js
@@ -4,33 +4,40 @@ Functions used to teleport a certain player to a specific station
 thanks jannes ;)
 */
 
-warpShipRangeteam1 = function(ship) {
-  x1 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.cos((game.step/216000+(1/3))*Math.PI*2);
-  y1 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.sin((game.step/216000+(1/3))*Math.PI*2);
-  ship.set({x:x1,y:y1,vx:0,vy:0}) ;
-  game.addCollectible({code:12,x:-960,y:-999});
-} ;
+function OrangeBase() {
+  let x = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.cos((game.step / 216000 + (2 / 2)) * Math.PI * 2);
+  let y = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.sin((game.step / 216000 + (2 / 2)) * Math.PI * 2);
+  return [x, y];
+}
 
-warpShipRangeteam2 = function(ship) {
-  x2 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.cos((game.step/216000+(2/3))*Math.PI*2);
-  y2 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.sin((game.step/216000+(2/3))*Math.PI*2);
-  ship.set({x:x2,y:y2,vx:0,vy:0}) ;
-} ;
+function GreenBase() {
+  let x = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.cos((game.step / 216000 + (1 / 3)) * Math.PI * 2);
+  let y = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.sin((game.step / 216000 + (1 / 3)) * Math.PI * 2);
+  return [x, y];
+}
 
-warpShipRangeteam3 = function(ship) {
-  x1 = ((Math.sqrt(2)/2)*game.options.map_size*10/2+50)*Math.cos((game.step/216000+(1/2))*Math.PI*2);
-  y1 = ((Math.sqrt(2)/2)*game.options.map_size*10/2+50)*Math.sin((game.step/216000+(1/2))*Math.PI*2);
-  ship.set({x:x1,y:y1,vx:0,vy:0}) ;
-} ;
+function BlueBase() {
+  let x = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.cos((game.step / 216000 + (2 / 3)) * Math.PI * 2);
+  let y = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.sin((game.step / 216000 + (2 / 3)) * Math.PI * 2);
+  return [x, y];
+}
 
-warpShipRangeteam4 = function(ship) {
-  x3 = ((Math.sqrt(2)/2)*game.options.map_size*10/2+50)*Math.cos((game.step/216000+(2/2))*Math.PI*2);
-  y3 = ((Math.sqrt(2)/2)*game.options.map_size*10/2+50)*Math.sin((game.step/216000+(2/2))*Math.PI*2);
-  ship.set({x:x3,y:y3,vx:0,vy:0}) ;
-} ;
+function PinkBase() {
+  let x = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.cos((game.step / 216000 + (1 / 2)) * Math.PI * 2);
+  let y = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.sin((game.step / 216000 + (1 / 2)) * Math.PI * 2);
+  return [x, y];
+}
 
-warpShipRangeteam5 = function(ship) {
-  x4 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.cos((game.step/216000+(2/3))*Math.PI*2);
-  y4 = ((Math.sqrt(2)/2)*game.options.map_size*10/2)*Math.sin((game.step/216000+(2/3))*Math.PI*2);
-  ship.set({x:x4,y:y4,vx:0,vy:0}) ;
-} ;
+function YellowBase() {
+  let x = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.cos((game.step / 216000 + (2 / 2)) * Math.PI * 2);
+  let y = ((Math.sqrt(2) / 2) * game.options.map_size * 5) * Math.sin((game.step / 216000 + (2 / 2)) * Math.PI * 2);
+  return [x, y];
+}
+
+const bases = [OrangeBase, GreenBase, BlueBase, PinkBase, YellowBase];
+tpto = function(what) {
+  if (what >= 0 && what < bases.length) {
+    const baseCoordinates = bases[what]();
+    game.ships[0].set({x: baseCoordinates[0], y: baseCoordinates[1]});
+  }
+}
