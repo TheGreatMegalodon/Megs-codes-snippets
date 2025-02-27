04ff284cc7503c48fcf728d6d4acc70b405542f4
TheGreatMegalodon <69216391+TheGreatMegalodon@users.noreply.github.com>
2025-02-27 23:38:04 +0100

    Update playersIDList.js

--- playersIDList.js
+++ playersIDList.js
@@ -1,7 +1,11 @@
 /*
-Copy that in the console when the mode is running and it will give you every players name+ID that are currently in the game
-the ID is the used in commands usally like this:
+Copy that in the console when the mode is running.
+It will then give you a list of every players (name+ID) that are currently in the game.
+
+The ID is frequently used in commands like this:
     game.findShip(ID).set({type: 101, crystals: 999, shield: 10, x: 0, y: 0});
 */
 
-game.modding.terminal.echo(`Total number of players: ${game.ships.length}\n\nPlayers:\n${game.ships.map(ship => {`Name: ${ship.name} ID: ${ship.id}`;}).join("\n")}\n`);
+const amount = game.ships.length;
+const players = game.ships.map(ship => `Name: ${ship.name} ID: ${ship.id}`).join("\n");
+game.modding.terminal.echo(`Total number of players: ${amount}\n\nPlayers:\n${players}\n`);
